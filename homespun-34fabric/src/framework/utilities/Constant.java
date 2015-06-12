a2c220ef573e0f5c6686068244e527ce998a4d06
Jyothy Mariam John <jyothym@gmail.com>
2015-06-12 18:55:49 +0530

    checking in user credentials for the test site

--- homespun-34fabric/src/framework/utilities/Constant.java
+++ homespun-34fabric/src/framework/utilities/Constant.java
@@ -3,6 +3,6 @@ package framework.utilities;
 public class Constant {
 	public static final String Browser = "chrome";
     public static final String BaseURL = "http://www.store.demoqa.com";
-    public static final String Username = "testuser_1";
-    public static final String Password = "Test@123";
+    public static final String Username = "user1";
+    public static final String Password = "test123";
 }
